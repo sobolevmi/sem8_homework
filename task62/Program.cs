33c4a14d129922751cf5377c70dee8005847d6dc
Maksim Sobolev <sobmig@gmail.com>
2022-07-24 17:33:05 +0300

    завершено написание кода программы

---
+++ task62/Program.cs
@@ -0,0 +1,101 @@
+﻿// Заполнить спирально двумерный массив размерами 4x4
+int [,] GetArray (int size)
+{
+    int [,] result = new int [size, size];
+    int i = 0;
+    int j = 0;
+    int rowE = size - 1;
+    int columnE = size - 1;
+    int rowS = 0;
+    int columnS = 0;
+    bool left = true;
+    bool top = true;
+    int count = 0;
+    while (count < size * size)
+    {
+        count++;
+        result [i, j] = count;
+        if (left && top) // идем вправо
+        {
+            if (j == columnE)
+            {
+                rowS++;
+                top = true;
+                left = false;
+                i++;
+                continue;
+            }
+            else
+            {
+                j++;
+                continue;
+            }
+        }
+        if (!left && top) // идем вниз
+        {
+            if (i == rowE)
+            {
+                columnE--;
+                top = false;
+                left = false;
+                j--;
+                continue;
+            }
+            else
+            {
+                i++;
+                continue;
+            }
+        }
+        if (!left && !top) // идем влево
+        {
+            if (j == columnS)
+            {
+                rowE--;
+                top = false;
+                left = true;
+                i--;
+                continue;
+            }
+            else
+            {
+                j--;
+                continue;
+            }
+        }
+        if (left && !top) // идем вверх
+        {
+            if (i == rowS)
+            {
+                columnS++;
+                top = true;
+                left = true;
+                j++;
+                continue;
+            }
+            else
+            {
+                i--;
+                continue;
+            }
+        }
+    }
+    return result;
+}
+
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write(inArray[i, j] + "  ");
+        }
+        Console.WriteLine();
+    }
+}
+
+Console.Write ("Введите размер матрицы: ");
+int s = Convert.ToInt32 (Console.ReadLine ());
+int [,] userArray = GetArray (s);
+PrintArray (userArray);
